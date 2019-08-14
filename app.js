955ae6398635a1345795bdfa6f041c2ae8e13ecc
nextron <manisingh893@gmail.com>
2019-08-14 20:03:36 +0530

    added functionality to verify every request before it fetches details from the server

--- app.js
+++ app.js
@@ -12,20 +12,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(async (req) => {
+//To verify every req if user is logged in or not
+app.use(async (req,res,next) => {
     try {
-        const token = req.headers.authorization
+        const token = req.headers.authorization;
         const decoded = await jwt.verify(token, 'shhhhh')
         req.id = decoded.user;
-        return req.next()
+        next();
+        //return res.send("success")
     } catch (e) {
-        return req.next()
+        return res.send({success:"false",msg:"Please Login First"});
     }
 })
 
 //mongo connection
-let uri = "mongodb+srv://nextron:nextron@nextron-xwdga.mongodb.net/test?retryWrites=true&w=majority";
-//let uri = "mongodb://localhost:27017/rup"
+//let uri = "mongodb+srv://nextron:nextron@nextron-xwdga.mongodb.net/test?retryWrites=true&w=majority";
+let uri = "mongodb://localhost:27017/rup"
 
 mongoose.connect(uri, {useNewUrlParser: true}, function(err){
     if (err) console.log(err);
@@ -36,6 +38,7 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //console.log(db.products_inventory.find());
+
 
 //handle product inventory route
 const productInventoryRoute = require("./routes/productInventory.route");
